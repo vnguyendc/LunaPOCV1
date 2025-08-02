2bf78e648867159fa94fcf9845d7685a8fddc40e
Vinh Nguyen <vnguyendc@gmail.com>
2025-08-02 13:55:46 -0400

    Initial Commit

---
+++ LunaTests/LunaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LunaTests.swift
+//  LunaTests
+//
+//  Created by VinhNguyen on 8/2/25.
+//
+
+import Testing
+@testable import Luna
+
+struct LunaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
